98fdfd816801ab45fa5dec998999b9d0a3725282
kipdv67 <kipdv67@gmail.com>
2026-05-14 23:42:35 +0700

    upload code Kip

--- kthp_ltwn2/src/main/java/com/example/kthp_ltwn2/repository/AdvisorClassSectionRepository.java
+++ kthp_ltwn2/src/main/java/com/example/kthp_ltwn2/repository/AdvisorClassSectionRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
-
+//Kip
 @Repository
 public interface AdvisorClassSectionRepository extends JpaRepository<AdvisorClassSection, UUID> {
     Page<AdvisorClassSection> findByIsActiveTrue(Pageable pageable);
