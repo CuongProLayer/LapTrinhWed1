1aedc28f653964d106d314a0c3e23b36960f43d5
Doan Duc Vuong <vuongdoanduc46@gmail.com>
2026-05-14 09:11:47 +0700

    upload code

--- kthp_ltwn2/src/main/java/com/example/kthp_ltwn2/controller/MajorWebController.java
+++ kthp_ltwn2/src/main/java/com/example/kthp_ltwn2/controller/MajorWebController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.util.UUID;
-
+//Vuong
 @Controller
 @RequestMapping("/majors")
 public class MajorWebController {
