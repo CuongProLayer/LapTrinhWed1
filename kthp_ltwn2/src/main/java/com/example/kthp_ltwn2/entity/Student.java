270257b697511d0c82a4056f274afc531247271b
kipdv67 <kipdv67@gmail.com>
2026-05-14 23:21:28 +0700

    upload code Kip

--- kthp_ltwn2/src/main/java/com/example/kthp_ltwn2/entity/Student.java
+++ kthp_ltwn2/src/main/java/com/example/kthp_ltwn2/entity/Student.java
@@ -6,7 +6,7 @@ import org.hibernate.annotations.UuidGenerator;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
-
+//Kip
 @Entity
 @Table(name = "students")
 public class Student {
