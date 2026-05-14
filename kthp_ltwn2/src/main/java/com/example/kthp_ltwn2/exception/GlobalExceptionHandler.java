559e63f4cb2e2b4e903f6257b09894e7077fa60e
Doan Duc Vuong <vuongdoanduc46@gmail.com>
2026-05-14 09:17:04 +0700

    upload code

--- kthp_ltwn2/src/main/java/com/example/kthp_ltwn2/exception/GlobalExceptionHandler.java
+++ kthp_ltwn2/src/main/java/com/example/kthp_ltwn2/exception/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-//
+
 import java.util.stream.Collectors;
 
 @RestControllerAdvice(annotations = RestController.class)
