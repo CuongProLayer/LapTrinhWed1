b506c055623dd95d17517e897bdc73b95589a01f
Cuong <cuong@gmail.com>
2026-05-15 20:51:59 +0700

    upload code Cuong

--- kthp_ltwn2/src/main/java/com/example/kthp_ltwn2/service/MajorServiceImpl.java
+++ kthp_ltwn2/src/main/java/com/example/kthp_ltwn2/service/MajorServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.UUID;
-
+//
 @Service
 public class MajorServiceImpl implements MajorService {
     private final MajorRepository repository;
